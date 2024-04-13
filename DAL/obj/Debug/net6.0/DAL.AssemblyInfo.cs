576923dfe95f84c0882391ace669cf8b6a551002
Gaurav Verma <153429649+developergauravverma@users.noreply.github.com>
2024-04-13 22:47:36 +0530

    startup code

--- DAL/obj/Debug/net6.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net6.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+299e065cfd1beeb50e463d623a2968a7820b08ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
